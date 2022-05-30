41db26ce341f8418067682ab1101d30d2708df26
EvhenKyrychenko <genius61161@gmail.com>
2022-05-31 01:45:42 +0300

    Update gallery.js

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -7,21 +7,6 @@ function myFunction(item) {
         ev.currentTarget.classList.add('active');
     });
     item.addEventListener('mouseleave', function (ev) {
-    ev.currentTarget.classList.remove('active');
+        ev.currentTarget.classList.remove('active');
  });
-}
-;   
-    
-// add.addEventListener('hover', () => {
-//     active.classList.add('hover');
-// 
-
-// document.querySelector('gallery__item').classList.add('hover');
-
-// document.querySelector('.gallery__item').addEventListener('mouseenter', function (ev) {
-//     ev.currentTarget.classList.add('active');
-// });
-// document.querySelector('.gallery__item').addEventListener('mouseleave', function (ev) {
-//     ev.currentTarget.classList.remove('active');
-//  });
-
+};
