5707218efae8be817afdfbcf98dd3c7e93e5321e
Zhenya Pastukhov <amamahahatala@hotmail.com>
2022-05-31 00:10:06 +0300

    try it

---
+++ src/js/hidden-header.js
@@ -0,0 +1,23 @@
+let lastScroll = 0;
+const defaultOffset = 200;
+const header = document.querySelector('.header');
+
+const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
+const containHide = () => header.classList.contains('hide');
+
+window.addEventListener('scroll', () => {
+
+    if (scrollPosition() > lastScroll && !containHide()) {
+        //scroll down
+        header.classList.add('hide');
+        console.log('down');
+    }
+    else if (scrollPosition() < lastScroll && containHide()) {
+        //scroll up
+        console.log('up');
+        header.classList.remove('hide');
+    }
+
+    lastScroll = scrollPosition();
+
+})
