d1fd65cf80f87e2552de605edff7abb0ba4631b5
Pavlo Myliutyn <pasam9300@gmail.com>
2022-05-31 21:12:01 +0200

    update
    Зробив молоко
    доробив модалку, залишилось текст

--- src/js/modal__howmade.js
+++ src/js/modal__howmade.js
@@ -1,15 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('[data-modal-open-howmade]'),
+    closeModalBtn: document.querySelector('[data-modal-close-howmade]'),
+    modal: document.querySelector('[data-modal-howmade]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    document.body.classList.toggle('modal-open');
     refs.modal.classList.toggle('is-hidden');
   }
 })();
