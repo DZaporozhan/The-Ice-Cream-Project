67b37aa4f6e5c538134e549a2d3a0166de8dc739
Pavlo Myliutyn <pasam9300@gmail.com>
2022-06-01 18:50:04 +0200

    Update
    Update Modal Howmade

--- src/js/modal__howmade.js
+++ src/js/modal__howmade.js
@@ -1,14 +1,17 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open-howmade]'),
+    openModalBtnHeader: document.querySelector('[data-modal-open-header]'),
     closeModalBtn: document.querySelector('[data-modal-close-howmade]'),
     modal: document.querySelector('[data-modal-howmade]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtnHeader.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
+    document.body.classList.toggle('no-scroll');
     refs.modal.classList.toggle('is-hidden');
   }
 })();
